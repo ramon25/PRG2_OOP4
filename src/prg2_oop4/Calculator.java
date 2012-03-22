b88a24efac9553f0a8ead14bebad21f467cf70e8
ramon25 <ramon25@gmail.com>
2012-03-22 10:17:03 +0100

    First Commit

---
+++ src/prg2_oop4/Calculator.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prg2_oop4;
+
+import java.awt.Frame;
+
+/**
+ *
+ * @author ramon
+ */
+public class Calculator extends Frame {
+    
+}
